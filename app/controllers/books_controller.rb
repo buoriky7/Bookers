a51c1da0e19efa482e1667afeeb4e40d9fc4811b
kentaroyajima <buoriky7@gmail.com>
2019-04-14 20:18:16 +0900

    Bookers1kadai

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+  def index
+  	@book = Book.new
+  	@books = Book.all
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	if book.save
+      flash[:notice] = "successfully"
+    end
+
+    redirect_to book_path(book.id)
+    
+  end
+
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully"
+    end
+
+    redirect_to book_path(@book.id)
+
+  end
+
+  def destroy
+    @books = Book.find(params[:id])
+    @books.destroy
+    redirect_to books_path
+  end
+
+   private
+    def book_params
+        params.require(:book).permit(:title, :body)
+    end
+end
